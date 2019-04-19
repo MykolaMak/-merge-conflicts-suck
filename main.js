78d2f8a4a514d8f0174c5491b9634c4db6b37dd1
MakolaMak <makolamak@gmail.com>
2019-04-19 13:28:03 +0300

    maybe i going right way now

--- main.js
+++ main.js
@@ -1,6 +1,2 @@
 var num = 50;
-<<<<<<< HEAD
-var truth = "Hello Flatter Earth!"
-=======
-var truth = "Hello Flat Earth!"
->>>>>>> fee88442743a59b8f9a5f547327391681c3289fc
+var truth = "Hello Cats!"
